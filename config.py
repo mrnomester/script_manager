6adf089e5c7659ceebb2d89f1e65aae6da4b673b
Кодельник Максим Сергеевич <kodelnik_ms@avuar.local>
2025-08-05 12:25:30 +0500

    создано окно и файл конфигурации

---
+++ config.py
@@ -0,0 +1,3 @@
+import os
+
+script_folder = os.path("\\nas\Distrib\script\.manual_scripts")
--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-qwe
+import tkinter as tk
+import tkinter.ttk as ttk
+import os
+from symtable import Class
+
+class ScriptManager(tk.Tk):
+    def __init__(self):
+
+    def button:
+        #кнопка закрытия окна
+        btn_close = tk.Button(root, text="Закрыть окно", command=root.destroy)
+        btn_close.pack()
+
+        #кнопка открытия папки (\\nas\Distrib\script\.manual_scripts)
+        btn_open_folder = tk.Button(root, text="Открыть папку со скриптами")
+    def _main_:
+        root = tk.Tk()
+        root.title("Script manager")
+        root.geometry("1200x800")
+        root.mainloop()
