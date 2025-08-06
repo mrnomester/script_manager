83070d2f17110c2d224efd6d6907321c9c0bcc6f
Кодельник Максим Сергеевич <kodelnik_ms@avuar.local>
2025-08-06 14:20:43 +0500

    Интерфейс без привязки логики

--- main_window.py
+++ main_window.py
@@ -1,31 +1,75 @@
-from PySide6.QtWidgets import QMainWindow, QWidget, QVBoxLayout
-from PySide6.QtWidgets import QListWidget
+from PySide6.QtWidgets import (
+    QApplication, QMainWindow, QWidget,
+    QVBoxLayout, QLabel, QListWidget, QPushButton
+)
 import sys
+from logic import script_lists, start_script
 
-'''
-class MainWindow(QMainWindow):
+
+class ScriptManager(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Script Manager")
-        self.resize(800, 600)
+        #self.setWindowIcon(QIcon("icon.ico"))
+        self.resize(500, 600)
 
-        central_widget = QWidget(self)
+        self.list_widget = None
+        self.button = None
+        self.status_bar = None
+
+        self.setup_ui()
+
+    def setup_ui(self):
+        # Центральный виджет
+        central_widget = QWidget()
         self.setCentralWidget(central_widget)
-        self.layout = QVBoxLayout(central_widget)
 
-def main():
+        #Лэйауты
+        layout = QVBoxLayout()
+        central_widget.setLayout(layout)
+        layout.setContentsMargins(10, 10, 10, 10)  # Отступы: лево, верх, право, низ
+        layout.setSpacing(10)  # Расстояние между элементами
+
+        # Подпись в лэайаут скриптов
+        label = QLabel("Скрипты:")
+        layout.addWidget(label)
+
+        # Поле, где будет список скриптов
+        self.list_widget = QListWidget()
+        layout.addWidget(self.list_widget)
+
+        # Загружаем скрипты
+        scripts = script_lists()
+        self.list_widget.addItems(scripts)
+        self.list_widget.itemSelectionChanged.connect(self.on_selection_changed)
+
+        # Кнопка запуска скрипта
+        self.button = QPushButton("Запустить")
+        self.button.clicked.connect(self.on_button_clicked)
+        self.button.setEnabled(False)
+        layout.addWidget(self.button)
+
+        # Статус бар
+        self.status_bar = self.statusBar()
+        self.status_bar.setSizeGripEnabled(True)
+        self.status_bar.setStyleSheet("QStatusBar{background-color:rgb(0, 0, 0)}")
+        self.status_bar.showMessage("Готово")
+
+    def on_selection_changed(self):
+        self.button.setEnabled(len(self.list_widget.selectedItems()) > 0)
+
+    def on_button_clicked(self):
+        selection = self.list_widget.selectedItems()
+        if selection:
+            self.status_bar.showMessage(f"Выбран {selection[0].text()}")
+        else:
+            self.status_bar.showMessage("Готово ")
+
+
+# Основное окно
+if __name__ == "__main__":
     app = QApplication(sys.argv)
-    window = MainWindow()
+    app.setStyle("Fusion")
+    window = ScriptManager()
     window.show()
     app.exec()
-
-if __name__ == '__main__':
-    main(app)
-
-'''
-app = QApplication(sys.argv)
-window = QMainWindow()
-window.setWindowTitle("Script Manager")
-window.resize(800, 600)
-window.show()
-app.exec_()
